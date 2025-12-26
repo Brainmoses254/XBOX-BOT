4d5d80804d566176fe83096f9f8e461c15a80936
Brainmoses254 <joinmoses254@gmail.com>
2025-12-26 12:33:48 +0300

    Update setting.js

--- setting.js
+++ setting.js
@@ -1,8 +1,8 @@
 //=====💫 Xbox bot🔥====
 
 module.exports = {
-  BOT_NAME: 'sᴛᴀʀ xᴅ', // ur bot name
-  OWNER_NAME: 'ᴍᴀʟᴠɪɴ ᴋɪɴɢ', //your name
+  BOT_NAME: 'Xbox bot', // ur bot name
+  OWNER_NAME: 'Xbox bot', //your name
   OWNER_NUMBER: '254710329917', // add your phone number
   SESSION_ID: '', // use sessin id if inbuilt pairing not working
   MODE: "public", // public or private
